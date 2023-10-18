1d4d470044c3beb45065b8a2cb173f8b42904d04
Naufff <114640395+Naufff@users.noreply.github.com>
2023-10-18 23:43:47 +0700

    Tugas 2

---
+++ Tugas2/Matematika.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author naufa
+ */
+public class Matematika {
+     public int pertambahan(int a, int b) {
+        return a + b;
+     }
+
+     public int pengurangan(int a, int b) {
+        return a - b;
+     }
+
+     public int perkalian(int a, int b) {
+        return a * b;
+     }
+
+     public float pembagian(int a, int b) {
+        return (float) a / b;
+     }
+}
---
+++ Tugas2/bingobing.java
@@ -0,0 +1,7 @@
+public class bingobing {
+    public static void main(String[] args){
+            bingo showw = new bingo();
+            
+            showw.vieww();
+    }
+}
